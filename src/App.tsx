7cf5065fe26fe0bb89c9829fde14facb4b1646cb
yelee park <dev.yelee@gmail.com>
2022-06-10 21:45:52 +0900

    global style 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="App">hi</div>;
+  return <div>hi</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import GlobalStyle from './style/globalStyle';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
